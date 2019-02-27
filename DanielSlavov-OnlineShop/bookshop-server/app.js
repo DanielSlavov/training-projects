ea25dc0182d0f2f457ed48a8422f4d81a748ca96
user <daxazzbg@gmail.com>
2019-02-27 18:27:38 +0200

    added /GET routes and return data from db

--- DanielSlavov-OnlineShop/bookshop-server/app.js
+++ DanielSlavov-OnlineShop/bookshop-server/app.js
@@ -1,6 +1,6 @@
 const {Pool} = require('pg')
 const express = require("express")
-const {Product} = require( "./model/Product.js")
+const Product = require( "./model/Product.js")
 
 const app = express()
 const port=3002
@@ -13,25 +13,84 @@ const dbClient=new Pool({
 })
 
 app.get('/getBooks',(req,res)=>{
-    Product.getProduct(
-        dbClient,
-        req.query.cat_id,
-        req.query.subcat_id,
-        req.query.man_id)
-        .then(dbRes=>{
+    Product.getBooks(
+        dbClient,{
+            genre_id:req.query.genre_id,
+            author_id:req.query.author_id,
+            publisher_id:req.query.publisher_id
+        }).then(dbRes=>{
             res.send(dbRes.rows)
-        }).catch(err=>{
-            console.log(err)
         })
 })
-
-
-
-app.get('/getGenres',(req,res)=>{
-
-    
+app.get('/getMerch',(req,res)=>{
+    Product.getMerch(dbClient,{
+        category_id:req.query.category_id,
+        manufacturer_id:req.query.manufacturer_id
+    }).then(dbRes=>{
+        res.send(dbRes.rows)
+    })
 })
 
-app.get('')
+app.get('/getGenres',(req,res)=>{
+    Product.getFromTable(dbClient,"Genres")
+    .then(dbRes=>{
+        res.send(dbRes.rows)
+    })
+})
+app.get('/getGenre',(req,res)=>{
+    Product.getFromTableById(dbClient,"Genres",req.query.params.id)
+    .then(dbRes=>{
+        res.send(dbRes.rows[0])
+    })
+})
+app.get('/getAuthors',(req,res)=>{
+    Product.getFromTable(dbClient,"Authors")
+    .then(dbRes=>{
+        res.send(dbRes.rows)
+    })
+})
+app.get('/getAuthor',(req,res)=>{
+    Product.getFromTableById(dbClient,"Authors",req.query.params.id)
+    .then(dbRes=>{
+        res.send(dbRes.rows[0])
+    })
+})
+app.get('/getPublishers',(req,res)=>{
+    Product.getFromTable(dbClient,"Publishers")
+    .then(dbRes=>{
+        res.send(dbRes.rows)
+    })
+})
+app.get('/getPublisher',(req,res)=>{
+    Product.getFromTableById(dbClient,"Publishers",req.query.params.id)
+    .then(dbRes=>{
+        res.send(dbRes.rows[0])
+    })
+})
+app.get('/getManufacturers',(req,res)=>{
+    Product.getFromTable(dbClient,"Manufacturers")
+    .then(dbRes=>{
+        res.send(dbRes.rows)
+    })
+})
+app.get('/getManufacturer',(req,res)=>{
+    Product.getFromTableById(dbClient,"Manufacturers",req.query.params.id)
+    .then(dbRes=>{
+        res.send(dbRes.rows[0])
+    })
+})
+app.get('/getCategories',(req,res)=>{
+    Product.getFromTable(dbClient,"Categories")
+    .then(dbRes=>{
+        res.send(dbRes.rows)
+    })
+})
+app.get('/getCategory',(req,res)=>{
+    Product.getFromTableById(dbClient,"Categories",req.query.params.id)
+    .then(dbRes=>{
+        res.send(dbRes.rows[0])
+    })
+})
+
 
 app.listen(port,()=>console.log('listening on '+port))
--- DanielSlavov-OnlineShop/bookshop-server/model/Product.js
+++ DanielSlavov-OnlineShop/bookshop-server/model/Product.js
@@ -1,47 +1,90 @@
+const isRequired=()=>{throw new error("param is required")}
 class Product{
     constructor(obj){
         this.id=obj.id
         this.name=obj.name
-        this.manufacturer_id=obj.manufacturer_id
-        this.subcategory_id=obj.subcategory_id
-        this.supplier_id=obj.supplier_id
-        this.description=obj.description
         this.price=obj.price
         this.quantity=obj.quantity
     }
     
-    static getProduct(dbClient,cat_id,subcat_id,man_id){
-        var query="SELECT * from Products "
-        var params=[]
+    static getBooks(dbClient,filter_obj={genre_id,author_id,publisher_id}){
+        var query="SELECT * from Products P LEFT JOIN Books B on P.id=B.product_id"
+
+        if(filter_obj!={})
+        {
+            var params=[]
+            if(filter_obj.category_id){
+                params.push("P.genre_id="+filter_obj.cat_id)
+            }
+            if(filter_obj.author_id){
+                params.push("B.author_id="+filter_obj.author_id)
+            }
+            if(filter_obj.publisher_id){
+                params.push("B.publisher_id="+filter_obj.publisher_id)
+            }
+            if(params.length>0){
+                query+=" WHERE "+ params.join(" AND ")
+            }
+        }
+        return dbClient.query(query)
+    }
+
+    static getMerch(dbClient,filter_obj={category_id,manufacturer_id}){
+        var query="SELECT * FROM Products P LEFT JOIN Merch M on P.id=M.product_id"
         
-        if(cat_id){
-            params.push("S.category_id="+cat_id)
-        }
-        if(subcat_id){
-            params.push("S.id="+subcat_id)
-        }
-        if(man_id){
-            params.push("P.manufacturer_id="+man_id)
-        }
-        if(params.length>0){
-            query+="P INNER JOIN subcategories S on P.subcategory_id=S.id WHERE "+ params.join(" AND ")
+        if(filter_obj!={}){
+            var params=[]
+            if(filter_obj.category_id){
+                params.push("P.category_id="+filter_obj.category_id)
+            }
+            if(filter_obj.manufacturer_id){
+                params.push("P.manufacturer_id="+filter_obj.manufacturer_id)
+            }
+            if(params.length>0){
+                query+=" WHERE "+ params.join(" AND ")
+            }
         }
         return dbClient.query(query)
-        //return query
     }
-
-}
-
-class Category{
-    constructor(obj){
-        this.id=obj.id
-        this.name=obj.name
-    }
-    static getId(dbClient,categoryName){
-        if(!categoryName) return false
-        var query="Select ID from Categories WHERE name="+categoryName
+    static getBookById(dbClient,id){
+        //By product id
+        var query="SELECT * FROM Books B FULL JOIN Products P on B.product_id=P.id WHERE P.id="+id
         return dbClient.query(query)
     }
+    static getMerchById(dbClient,id){
+        //By product id
+        var query="SELECT * FROM Merch M FULL JOIN Products P on M.product_id=P.id WHERE P.id="+id
+        return dbClient.query(query)
+    }
+    static getImages(dbClient,id){
+        //By product id
+        var query = "SELECT * FROM Images WHERE product_id="+id;
+        return dbClient.query(query);
+    }
+    static getFromTable(dbClient,tableName=isRequired()){
+        var query="SELECT * FROM "+tableName;
+        return dbClient.query(query)
+    }
+    static getFromTableById(dbClient,tableName=isRequired(),id){
+        if(!id) return new Promise((res,rej)=>{res.send("")})
+        var query="SELECT * FROM "+tableName;
+        query+=" WHERE id="+id;
+        return dbClient.query(query)
+    }
+
+
+    static addBook(dbClient,{name,author_id,publisher_id,genre_id,description,price,quantity}){
+        //TODO
+    }
+    static addMerch(dbClient,{name,manufacturer_id,category_id,genre_id,description,price,quantity}){
+        //TODO
+    }
+    static addToTable(dbCliend,tableName=isRequired(),{name}){//ONLY FOR ENUM TABLES!
+        //if table is NOT ENUM(has only 'id' and 'name' columns)
+        //you should create a separate function
+
+        //TODO
+    }
 }
-module.exports.Product=Product
-module.exports.Category=Category
+module.exports=Product
+
